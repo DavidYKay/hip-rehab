ff1c55312f499089f56a0210aeb0208efc08acbc
David Young-Chan Kay <davidykay@gmail.com>
2014-09-21 10:52:41 -0400

    patient list is now populated by Firebase. so cool

--- web/app/views/PatientList.js
+++ web/app/views/PatientList.js
@@ -11,21 +11,22 @@ angular.module('myApp.PatientList', ['ngRoute'])
 
 .controller('PatientListCtrl', function PatientListCtrl($scope, $firebase, Page) {
   Page.setTitle('Patient List');
-  $scope.patients = [
-    {name: "Suzie Q", id: 1},
-    {name: "Joe Bloggs", id: 2},
-    {name: "David Kay", id: 3},
-  ];
+  // $scope.patients = [
+  //   {name: "Suzie Q", id: 1},
+  //   {name: "Joe Bloggs", id: 2},
+  //   {name: "David Kay", id: 3},
+  // ];
 
 
-    //var ref = new Firebase("https://glowing-fire-8184.firebaseio.com/patients");
-    //// create an AngularFire reference to the data
-    //var sync = $firebase(ref);
-    //// download the data into a local object
-    //$scope.data = sync.$asObject();
-    //// synchronize the object with a three-way data binding
-    //// click on `index.html` above to see it used in the DOM!
-    //syncObject.$bindTo($scope, "data");
-    //$scope.patients = sync.$asArray();
+  var ref = new Firebase("https://glowing-fire-8184.firebaseio.com/patients");
+  // create an AngularFire reference to the data
+  var sync = $firebase(ref);
+  // download the data into a local object
+  var syncObject = sync.$asObject();
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
+  $scope.patients = sync.$asArray();
+
 }
 );
