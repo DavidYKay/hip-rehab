ea544f4febf2b250fd141070e40880f05d5772eb
David Young-Chan Kay <davidykay@gmail.com>
2014-09-21 10:46:45 -0400

    hello world for firebase.

--- web/app/views/PatientDetail.js
+++ web/app/views/PatientDetail.js
@@ -10,7 +10,18 @@ angular.module('myApp.PatientDetail', ['ngRoute'])
 }])
 
 .controller('PatientDetailCtrl',
-  function PatientDetailCtrl($scope) {
+  function PatientDetailCtrl($scope, $firebase, Page) {
     $scope.patient = {name: "David Kay", id: 3};
+    Page.setTitle('Patient Detail');
+
+    var ref = new Firebase("https://glowing-fire-8184.firebaseio.com/data");
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+    // download the data into a local object
+    var syncObject = sync.$asObject();
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObject.$bindTo($scope, "data");
+
   }
 );
