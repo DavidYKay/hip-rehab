fee3ac67e2f35dae8224170518aacbe4dd673c93
David Young-Chan Kay <davidykay@gmail.com>
2014-09-21 12:50:03 -0400

    now showing check and x mark on patient list.

--- web/app/views/PatientList.js
+++ web/app/views/PatientList.js
@@ -11,11 +11,6 @@ angular.module('myApp.PatientList', ['ngRoute'])
 
 .controller('PatientListCtrl', function PatientListCtrl($scope, $firebase, Page) {
   Page.setTitle('Patient List');
-  // $scope.patients = [
-  //   {name: "Suzie Q", id: 1},
-  //   {name: "Joe Bloggs", id: 2},
-  //   {name: "David Kay", id: 3},
-  // ];
 
 
   var ref = new Firebase("https://glowing-fire-8184.firebaseio.com/patients");
@@ -28,5 +23,38 @@ angular.module('myApp.PatientList', ['ngRoute'])
   //syncObject.$bindTo($scope, "data");
   $scope.patients = sync.$asArray();
 
-}
-);
+  $scope.getImagePath = function(result) {
+    if (result) {
+      return "img/green-check.png";
+    } else {
+      return "img/red-x.png";
+    }
+  };
+
+  $scope.getResults = function(patient) {
+
+    if (patient.id == "1") {
+
+    return [
+      {id: 1, result: true},
+      {id: 2, result: true},
+      {id: 3, result: true}
+    ];
+
+    } else if (patient.id == "2") {
+
+    return [
+      {id: 1, result: false},
+      {id: 2, result: false},
+      {id: 3, result: true}
+    ];
+
+    } else {
+    return [
+      {id: 1, result: false},
+      {id: 2, result: false},
+      {id: 3, result: false}
+    ];
+    }
+  };
+});
